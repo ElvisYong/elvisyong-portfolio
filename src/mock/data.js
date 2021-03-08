16fb8c9b8ed827eefe42e629afdb1245c53c0636
elvisyong <elvisyong@gmail.com>
2021-03-08 19:03:48 +0800

    Change misc grammar and typos.

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,15 +19,15 @@ export const heroData = {
 export const aboutData = {
   img: 'elvis.jpg',
   paragraphOne: 'I\'m a student from Singapore Management University majoring in Information Systems.',
-  paragraphTwo: 'I am currently proficient in web development',
-  paragraphThree: 'Exploring and learning how to use make good use of technology to solve exisiting problems.',
+  paragraphTwo: 'I am currently diving into the world of software development',
+  paragraphThree: 'Exploring and learning to make good use of technology to solve exisiting problems.',
   resume: '', // if no resume, the button will not show up
 };
 
 export const skillsData = {
   frontendDesc: 'I like to see interaction of data with front-end components bringing life to the browser',
-  frontendLang: 'React/React-Native, VueJS, HTML, Sass, CSS',
-  backendDesc: 'I work with servers and databases to allow communication of data for the front-end to work reliably.',
+  frontendLang: 'React/React-Native, VueJS, HTML, SASS, CSS',
+  backendDesc: 'I work with servers to allow communication of data for the front-end to work reliably.',
   backendLang: 'ASP.NET CORE MVC/API, ExpressJS, Flask'
 }
 
@@ -37,7 +37,7 @@ export const projectsData = [
     id: nanoid(),
     img: 'project.jpg',
     title: 'Placeholder project',
-    info: 'Projects are still wip will update with working projects once done.',
+    info: 'Projects section are still WIP, will update with working projects once it\'s done.',
     info2: '',
     url: '',
     repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
